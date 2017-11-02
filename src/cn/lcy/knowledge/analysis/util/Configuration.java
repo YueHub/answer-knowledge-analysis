8d5d6c825be8b729691c0e98341a301e2e4e70c5
Yue <li_chuan_yue@foxmail.com>
2017-11-02 13:37:37 +0800

    delete some unwanted code

--- src/cn/lcy/knowledge/analysis/util/Configuration.java
+++ src/cn/lcy/knowledge/analysis/util/Configuration.java
@@ -8,12 +8,6 @@ import java.util.Properties;
 
 public class Configuration {
 	
-	private static final String PROPERTY_FILE_NAME = "ontology-construction.properties";
-
-	public static void main(String args[]) throws IOException {
-		Configuration.propertiesLoader(PROPERTY_FILE_NAME);
-	}
-	
     public static Properties propertiesLoader(String fileName) throws IOException {
         // 文件在class的根路径  
         InputStream is = Configuration.class.getClassLoader().getResourceAsStream(fileName);  
